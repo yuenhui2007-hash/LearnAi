1c1ce209e3f0548895a2197854621eec1c76a521
Yuen Hui <yuenhui@learnai.edu>
2026-08-06 07:57:02 +0000

    Delete auth-guard.js and admin-guard.js completely

--- js/auth-guard.js
+++
@@ -1,9 +0,0 @@
-/* LearnAI Auth Guard - Redirect to login if not authenticated */
-(function(){
-  'use strict';
-  var AUTH_KEY = 'learnai_auth_user';
-  var user = localStorage.getItem(AUTH_KEY);
-  if (!user) {
-    window.location.replace('login.html?redirect=' + encodeURIComponent(window.location.pathname.split('/').pop()));
-  }
-})();
